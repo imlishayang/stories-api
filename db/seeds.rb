0b15081e2e265758155aa35b25f6237acc190dcf
Lisa Yang <lisayang041@gmail.com>
2022-06-21 21:20:29 +0800

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Story.create(name: Faker::Movies::Lebowski.quote, text: Faker::TvShows::TwinPeaks.quote )
+Story.create(name: Faker::Movies::Lebowski.quote, text: Faker::TvShows::TwinPeaks.quote )
+Story.create(name: Faker::Movies::Lebowski.quote, text: Faker::TvShows::TwinPeaks.quote )
+Story.create(name: Faker::Movies::Lebowski.quote, text: Faker::TvShows::TwinPeaks.quote )
+Story.create(name: Faker::Movies::Lebowski.quote, text: Faker::TvShows::TwinPeaks.quote )
